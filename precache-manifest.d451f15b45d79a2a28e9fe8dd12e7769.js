817d0c55a6aee9bc9c46bc953b7baa885a74af8a
jbratcher <jeremybratcher@gmail.com>
2019-03-19 19:05:59 -0400

    Updates

---
+++ precache-manifest.d451f15b45d79a2a28e9fe8dd12e7769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abbdb36b1252f151583b",
+    "url": "/VoteInfo/static/js/runtime~main.a848771c.js"
+  },
+  {
+    "revision": "5ff6ef128df1d560040d",
+    "url": "/VoteInfo/static/js/main.bab76729.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/VoteInfo/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "5ff6ef128df1d560040d",
+    "url": "/VoteInfo/static/css/main.ada1f8f9.chunk.css"
+  },
+  {
+    "revision": "48c2f47fbbaee4538b7df4856742f632",
+    "url": "/VoteInfo/index.html"
+  }
+];
