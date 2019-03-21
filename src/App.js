06bd4b723411727fe3130ba5ddbd6113785dd66f
jbratcher <jeremybratcher@gmail.com>
2019-03-20 21:46:59 -0400

    feat: address form input store, voter info fetch

--- src/App.js
+++ src/App.js
@@ -10,15 +10,44 @@ class App extends Component {
     this.state = {
 
       dataRecieved: null,
-      url: `https://www.googleapis.com/civicinfo/v2/elections?key=${apikey}`
+      electionsUrl: `https://www.googleapis.com/civicinfo/v2/elections?key=${apikey}`,
+      street1: "140 pope street",
+      street2: "apt. 1",
+      city: "louisville",
+      state: "kentucky",
+      country: "us",
 
     }
   }
 
+  handleInputChange = e => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = e => {
+    console.log(e.target.name);
+    this.fetchData();
+  }
+
   fetchData = () => {
 
-    fetch(this.state.url)
-      .then(res => console.log(res))
+    const {street1, city, state} = this.state;
+    let voterInfoUrl = `https://www.googleapis.com/civicinfo/v2/voterinfo?key=${apikey}&address=${street1}.${city} ${state}&electionId=2000`;
+
+    fetch(voterInfoUrl)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          dataRecieved: true,
+          data: data
+        })
+      })
+      .then(() => console.log(this.state.data))
+      .catch(error => console.log(error))
 
   }
 
@@ -46,32 +75,83 @@ class App extends Component {
 
           <form>
 
-            <section className="labelContainer">
+            <div className="form-group">
+              <label htmlFor="street1"></label>
+              <input
+                type="text"
+                className="form-control"
+                name="street1"
+                aria-describedby="street1Help"
+                placeholder="Street Address"
+                onChange={this.handleInputChange}
+              >
+              </input>
+              <small id="emailHelp" className="form-text text-muted"><em>ex. 1600 Pennsylvania Ave NW</em></small>
+            </div>
 
-              <label htmlFor="street1">Street Address 1:</label>
-              <input type="text" name="street1"></input>
+            <div className="form-group">
+              <label htmlFor="street2"></label>
+              <input
+                type="text"
+                className="form-control"
+                name="street2"
+                aria-describedby="street2Help"
+                placeholder="Street Address cont."
+                onChange={this.handleInputChange}
+              >
+              </input>
+              <small id="street2Help" className="form-text text-muted"><em>ex. Apt. 231</em></small>
+            </div>
 
-              <label htmlFor="street2">Street Address 2:</label>
-              <input type="text" name="street2"></input>
+            <div className="form-group">
+              <label htmlFor="city"></label>
+              <input
+                type="text"
+                className="form-control"
+                name="city"
+                aria-describedby="cityHelp"
+                placeholder="City"
+                onChange={this.handleInputChange}
+              >
+              </input>
+              <small id="cityHelp" className="form-text text-muted"><em>ex. Washington</em></small>
+            </div>
 
-              <label htmlFor="city">City:</label>
-              <input type="text" name="city"></input>
+            <div className="form-group">
+              <label htmlFor="state"></label>
+              <input
+                type="text"
+                className="form-control"
+                name="state"
+                aria-describedby="state"
+                placeholder="State"
+                onChange={this.handleInputChange}
+              >
+              </input>
+              <small id="stateHelp" className="form-text text-muted"><em>ex. DC</em></small>
+            </div>
 
-              <label htmlFor="state">State:</label>
-              <input type="text" name="state"></input>
 
-              <label htmlFor="zipcode">Zip Code:</label>
-              <input type="text" name="zipcode"></input>
-
-            </section>
-
-            <button type="submit">Get Info</button>
-
-            { dataRecieved ?
-              <p>Data Output</p>
-            : null}
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onChange={this.handleSubmit}
+            >
+              Submit
+            </button>
 
           </form>
+
+          { dataRecieved ?
+            <ul>
+              <li>{this.state.data.normalizedInput.line1}</li>
+              <li>{this.state.data.normalizedInput.city}</li>
+              <li>{this.state.data.normalizedInput.state}</li>
+              <li>{this.state.data.normalizedInput.zip}</li>
+            </ul>
+          : null}
+
+
 
         </main>
 
