c486eb1c9c3c62e9a14af927d3a0d9067b5a06b4
Jeremy Bratcher <jeremybratcher@gmail.com>
2019-03-27 11:46:19 +0000

    Updates

--- precache-manifest.6ace19a16ef48c89863fa815927de4fd.js
+++ precache-manifest.6ace19a16ef48c89863fa815927de4fd.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/VoteInfo/static/js/runtime~main.a848771c.js"
   },
   {
-    "revision": "58c3e2123fcb0fc57687",
-    "url": "/VoteInfo/static/js/main.b6ebcb35.chunk.js"
+    "revision": "fa87f81f7aadf2d85549",
+    "url": "/VoteInfo/static/js/main.2ab49eb2.chunk.js"
   },
   {
     "revision": "73080c8c5313bf1fb476",
     "url": "/VoteInfo/static/js/2.d519b009.chunk.js"
   },
   {
-    "revision": "58c3e2123fcb0fc57687",
-    "url": "/VoteInfo/static/css/main.4c4ead1c.chunk.css"
+    "revision": "fa87f81f7aadf2d85549",
+    "url": "/VoteInfo/static/css/main.9ae09374.chunk.css"
   },
   {
-    "revision": "fbee5e3e41afa5ac8709eb66878f9fa3",
+    "revision": "8463b72b096265321f3804a53978740e",
     "url": "/VoteInfo/index.html"
   }
 ];
