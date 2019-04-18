46a6fca3003e684c40b4904184282a51de5b8236
Jeremy Bratcher <jeremybratcher@gmail.com>
2019-04-18 11:42:24 +0000

    Updates

--- precache-manifest.f07aa8052fd198afcdcbcb33e98e9a7e.js
+++ precache-manifest.f07aa8052fd198afcdcbcb33e98e9a7e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/VoteInfo/static/js/runtime~main.a848771c.js"
   },
   {
-    "revision": "c40ed74a8841ca50741d",
-    "url": "/VoteInfo/static/js/main.cc524060.chunk.js"
+    "revision": "c1673e8f1c14d3099bc3",
+    "url": "/VoteInfo/static/js/main.ac2a42e0.chunk.js"
   },
   {
     "revision": "73080c8c5313bf1fb476",
     "url": "/VoteInfo/static/js/2.d519b009.chunk.js"
   },
   {
-    "revision": "c40ed74a8841ca50741d",
-    "url": "/VoteInfo/static/css/main.4ed660f5.chunk.css"
+    "revision": "c1673e8f1c14d3099bc3",
+    "url": "/VoteInfo/static/css/main.eeabebee.chunk.css"
   },
   {
-    "revision": "6bf1810e232cd05200fa14d590e734dc",
+    "revision": "b3e24caa1fbc8f465c57fd86f20ac510",
     "url": "/VoteInfo/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/VoteInfo/precache-manifest.ec1a9ef24977b842180b4e51c2e76770.js"
+  "/VoteInfo/precache-manifest.f07aa8052fd198afcdcbcb33e98e9a7e.js"
 );
 
 workbox.clientsClaim();
