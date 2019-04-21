7b76edb6d2860aebb18525cf0cec09694c321196
Jeremy Bratcher <jeremybratcher@gmail.com>
2019-04-21 13:55:35 +0000

    refactor: mobile screen grid refactor

--- src/App.js
+++ src/App.js
@@ -124,33 +124,33 @@ class App extends Component {
 
         <header role="banner" aria-label="header">
 
-          <nav class="navbar navbar-expand-lg navbar-light">
+          <nav className="navbar navbar-expand-lg navbar-light">
           
-            <a class="navbar-brand card" id="brand" href="#top">
+            <a className="navbar-brand" id="brand" href="#top">
               <h1>VoteInfo</h1>
             </a>
             
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
           
-            <div class="collapse navbar-collapse card" id="mainNav">
-              <ul class="navbar-nav">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#top">Home <span class="sr-only">(current)</span></a>
+            <div className="collapse navbar-collapse" id="mainNav">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#top">Home <span className="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#top">Link</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#top">Link</a>
                 </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#top" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#top" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Dropdown
                   </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#navbarDropdown">Action</a>
-                    <a class="dropdown-item" href="#navbarDropdown">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#navbarDropdown">Something else here</a>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="#navbarDropdown">Action</a>
+                    <a className="dropdown-item" href="#navbarDropdown">Another action</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#navbarDropdown">Something else here</a>
                   </div>
                 </li>
               </ul>
